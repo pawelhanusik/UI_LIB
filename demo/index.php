38a7686dbc1c665440b11e3161e3e69927a2ef63
Pawel Hanusik <pawel@hanusik.pl>
2019-11-14 12:13:50 +0100

    Added minified version

--- demo/index.php
+++ demo/index.php
@@ -1,14 +1,36 @@
-<?php
-include_once("../ui_lib_min/ui.min.php");
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>UI_LIB Demo</title>
+	<meta name="description" content="UI_LIB Demo"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<?php
+		//Include UI_LIB in header in order to put all <style>s and <script>s into <head>
+		//(doing it in <body> should also work)
+		include_once("../ui_lib_min/ui.min.php");
+	?>
+</head>
+<body>
 
+<?php
 $names = explode("\n", file_get_contents("names.txt"));
 $surnames = explode("\n", file_get_contents("surnames.txt"));
 
+//Init progressBar and add tasks of waiting (in order to accomplish fake loading screen)
+$progressBar = new UI\ProgressBar();
+for($i = 0; $i < 100; ++$i){
+	$progressBar->addTask(function(){usleep(1000);}, 1);
+}
+
+//Init table
 $table = new UI\Table();
+//Set columns' names
 $table->setheader(
 	["ID", "Name", "Surname", "Year"]
 );
-
+//Add contents to the table
 $rows_count = 5627;
 $table->show(15);
 for($i = 0; $i < $rows_count; ++$i){
@@ -16,6 +38,25 @@ for($i = 0; $i < $rows_count; ++$i){
 		[$i, $names[rand(0, count($names)-1)], $surnames[rand(0, count($surnames)-1)], rand(1900, 2019)]
 	);
 }
+?>
 
+<!-- Draw progress bar into custom div positioned at the bottom -->
+<div style="
+	 position: fixed;
+	 top: 90%;
+	 width: 100%;
+">
+<?php
+	$progressBar->draw();
+?>
+</div>
+<?php
+//Execute progressBar
+$progressBar->execute();
+//Remove progressBar after waiting for 1 second in order to allow user to see 100%
+$progressBar->remove(1);
+
+//Draw table
 $table->draw();
 ?>
+</body>
--- ui_lib_min/ui.min.php
+++ ui_lib_min/ui.min.php
@@ -1,4 +1,4 @@
 <?php
-$ui_classes=["ui_table"];
+$ui_classes=["ui_table", "ui_progress_bar"];
 foreach($ui_classes as $filename){ include_once(realpath(dirname(__FILE__)) . "/classes.min/" .  $filename . ".min.php"); }
 ?>
